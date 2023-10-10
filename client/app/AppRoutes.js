8a33b64fa5675933d36db845cb65719b05276edb
Rachel Eckert <rachel.eckert13@gmail.com>
2023-10-10 12:06:57 -0400

    cleaned up boilerplate w/ no db

--- client/app/AppRoutes.js
+++ client/app/AppRoutes.js
@@ -1,45 +1,15 @@
-import React, { useEffect } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
-import AuthForm from '../features/auth/AuthForm';
-import Home from '../features/home/Home';
-import { me } from './store';
+import React, { useEffect } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { Route, Routes } from "react-router-dom";
 
 /**
  * COMPONENT
  */
 
 const AppRoutes = () => {
-  const isLoggedIn = useSelector((state) => !!state.auth.me.id);
-  const dispatch = useDispatch();
-
-  useEffect(() => {
-    dispatch(me());
-  }, []);
-
   return (
     <div>
-      {isLoggedIn ? (
-        <Routes>
-          <Route path="/*" element={<Home />} />
-          <Route to="/home" element={<Home />} />
-        </Routes>
-      ) : (
-        <Routes>
-          <Route
-            path="/*"
-            element={<AuthForm name="login" displayName="Login" />}
-          />
-          <Route
-            path="/login"
-            element={<AuthForm name="login" displayName="Login" />}
-          />
-          <Route
-            path="/signup"
-            element={<AuthForm name="signup" displayName="Sign Up" />}
-          />
-        </Routes>
-      )}
+      <h1>woof</h1>
     </div>
   );
 };
--- client/index.js
+++ client/index.js
@@ -1,16 +1,14 @@
-import React from 'react';
-import { createRoot } from 'react-dom/client';
-import { Provider } from 'react-redux';
-import store from './app/store.js';
-import App from './app/App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { Provider } from "react-redux";
 
-const root = createRoot(document.getElementById('app'));
+import App from "./app/App";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const root = createRoot(document.getElementById("app"));
 
 root.render(
   <Router>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </Router>
 );
